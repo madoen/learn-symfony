69007a75c0efc84ec450585d8e9f21a6f394f5d9
Ramdhan Hidayat <ramdan.hidayat@gmail.com>
2016-01-22 22:13:50 +0700

    #3rd lesson, understand how is controller worked: return new Response.

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+        return new Response('test Response');
     }
 }
