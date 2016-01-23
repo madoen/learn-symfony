04696204f8b0cbb66282614f345d3eb1cc41c929
Ramdhan Hidayat <ramdan.hidayat@gmail.com>
2016-01-23 10:01:14 +0700

    #8th lesson, understand twig: basic syntax.

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, $count)
     {
-        // Use render method from base controller on controller.php.
         return $this->render(
             'EventBundle:Default:index.html.twig',
-            array('name' => $name)
+            array('name' => $name, 'count' => $count)
         );
-        // This done the same way used on 2nd lesson.
     }
 }
