f5a8b4a6ec708cfd784de143821f9f9af8d90614
Ramdhan Hidayat <ramdan.hidayat@gmail.com>
2016-01-24 15:58:06 +0700

    #13th lesson, databases & doctrine: query and display our entity data.

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -8,9 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction($name, $count)
     {
+        // $em = $this->container->get('doctrine')->getManager();
+        // Can also be shortened to.
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Event');
+
+        $event = $repo->findOneBy(array(
+            'name' => 'Ramdhan\'s surprise birthday party'
+        ));
+
         return $this->render(
             'EventBundle:Default:index.html.twig',
-            array('name' => $name, 'count' => $count)
+            array('name' => $name, 'count' => $count, 'event' => $event)
         );
     }
 }
